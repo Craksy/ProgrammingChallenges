b0efdcc2de637596bd83d85ce1e51b5c0cdf24f4
craksy <craksyw@gmail.com>
2021-02-06 22:58:38 +0100

    Did challenge 05

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            for (int i = 1; i <= 100; i++) {
+                string? num = null;
+                if (i%3 == 0)
+                    num += "Fizz";
+                if (i % 5 == 0)
+                    num += "Buzz";
+                Console.WriteLine(num ?? i.ToString());
+            }
+        }
+    }
+}
---
+++ FizzBuzz/README.md
@@ -0,0 +1,27 @@
+﻿# 05 - FizzBuzz
+
+Not much to be said here. It's FizzBuzz.
+
+###### Example output
+```
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz...
+```
